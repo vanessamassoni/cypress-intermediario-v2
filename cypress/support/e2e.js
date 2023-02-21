b82dd5728498db940668bfeab3a8d3cd9bde7278
vanessa <vanemassoni@hotmail.com>
2023-02-21 11:00:49 -0300

    Alteracao

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import './gui_commands'
+import './api_commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
